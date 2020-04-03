5977d4dc60b58e5cf2d14f22638786a85423d1d0
Aman Shah <aman.shah96.as@gmail.com>
2020-04-03 19:38:19 -0400

    assignnent10

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,5 @@
 import json
-import boto3
+import boto3, time
 from boto3.dynamodb.conditions import Key,Attr
 
 def lambda_handler(event, context):
@@ -23,7 +23,7 @@ def lambda_handler(event, context):
 
 
     dynamo_table = 'Duedate'
-    dynamodb = boto3.resource(region_name="us-east-1")
+    dynamodb = boto3.resource('dynamodb',region_name="us-east-1")
 
     table = dynamodb.Table(dynamo_table)
 
@@ -34,34 +34,89 @@ def lambda_handler(event, context):
 
 
     for i in range(len(message)-1):
-        index="'"+i+"'"
-        bills=''
-        bills=','+bills.append(message[index])
+        index=str(i)
+        link='http://prod.amanshah.xyz/v1/bill/'+message[index]
+        link_array=[]
+        link_array.append(link)
 
 
 
+    csv = ',   '.join([str(elem) for elem in link_array])
     items = dynamo_row['Items']
 
 
     if not items:
         print("no similar items")
 
-
+        ttl=time.time()
+        print(ttl)
         response = table.put_item(
             Item={
-                'email': 'janedoe',
-                'first_name': 'Jane',
-                'last_name': 'Doe',
-                'age': 25,
-                'account_type': 'standard_user',
+                'email': emailid,
+                'links':csv,
+                'deleteTime':ttl
+
             }
         )
+
+
         print("New record inserted successfully")
-
-
-
-
+        prepare_and_send_email(emailid,csv)
+        print("New record email")
+    else:
+        print("record exists")
     return {
         'statusCode': 200,
         'body': json.dumps(message)
     }
+
+
+
+def prepare_and_send_email(recipient, body):
+    # Sender Email ID
+    SENDER = "noreply@" + 'prod.amanshah.xyz'
+
+
+    # The email body for recipients
+    BODY_TEXT = ('bill links' + "\r\n" +body)
+
+
+    SUBJECT = "link of bills"
+
+
+
+    # print(AWS_REGION)
+
+    # Create a new SES resource and specify a region.
+    client = boto3.client('ses',aws_region='us-east-1')
+    # Try to send the email.
+    CHARSET = "UTF-8"
+
+    try:
+        #Provide the contents of the email.
+        response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    recipient,
+                ],
+            },
+            Message={
+                'Body': {
+                    'Text': {
+                        'Charset': CHARSET,
+                        'Data': BODY_TEXT,
+                    },
+                },
+                'Subject': {
+                    'Charset': CHARSET,
+                    'Data': SUBJECT,
+                },
+            },
+            Source=SENDER,
+        )
+    # Display an error if something goes wrong.
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent!"),
+        print(response['MessageId'])
